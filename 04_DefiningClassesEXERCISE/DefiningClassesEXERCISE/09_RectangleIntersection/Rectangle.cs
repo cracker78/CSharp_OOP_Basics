475ca981fb275ed7041c0fa04f515cd06c4b168c
cracker78 <chavdar_vel@abv.bg>
2018-03-07 21:37:06 +0200

    up

---
+++ 04_DefiningClassesEXERCISE/DefiningClassesEXERCISE/09_RectangleIntersection/Rectangle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Rectangle
+{
+    private string id;
+    private double width;
+    private double height;
+    private double coordinateX;
+    private double coordinateY;
+
+    public Rectangle(string id, double width, double height, double coordinateX, double coordinateY)
+    {
+        this.Id = id;
+        this.Width = width;
+        this.Height = height;
+        this.CoordinateX = coordinateX;
+        this.CoordinateY = coordinateY;
+    }
+
+    public string Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public double Width
+    {
+        get { return width; }
+        set { width = value; }
+    }
+
+    public double Height
+    {
+        get { return height; }
+        set { height = value; }
+    }
+
+    public double CoordinateX
+    {
+        get { return coordinateX; }
+        set { coordinateX = value; }
+    }
+    public double CoordinateY
+    {
+        get { return coordinateY; }
+        set { coordinateY = value; }
+    }
+
+    public bool IntersectsRectangle(Rectangle r)
+    {
+        return this.ContainsRectangleCorner(r) || r.ContainsRectangleCorner(this);
+    }
+
+    private bool ContainsRectangleCorner(Rectangle r)
+    {
+        return this.ContainsPoint(r.coordinateX, r.coordinateY) ||
+               this.ContainsPoint(r.coordinateX, r.coordinateY + height) ||
+               this.ContainsPoint(r.coordinateX + width, r.coordinateY) ||
+               this.ContainsPoint(r.coordinateX + width, r.coordinateY + height);
+    }
+
+    private bool ContainsPoint(double x, double y)
+    {
+        return x >= this.coordinateX && x <= this.coordinateX + width &&
+               y >= this.coordinateY && y <= this.coordinateY + height;
+    }
+
+}
---
+++ 04_DefiningClassesEXERCISE/DefiningClassesEXERCISE/09_RectangleIntersection/RectangleIntersection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RectangleIntersection
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split().ToArray();
+        List<Rectangle> rectangles=new List<Rectangle>();
+
+        int numberOfRectangles = int.Parse(input[0]);
+        int intersectionChecks = int.Parse(input[1]);
+
+        for (int i = 0; i < numberOfRectangles; i++)
+        {
+            string[] args = Console.ReadLine().Split().ToArray();
+
+            string id = args[0];
+            double width = double.Parse(args[1]);
+            double height = double.Parse(args[2]);
+            double coordinateX = double.Parse(args[3]);
+            double coordinateY = double.Parse(args[4]);
+
+            var rectangle=new Rectangle(id, width,height,coordinateX,coordinateY);
+
+            rectangles.Add(rectangle);
+        }
+
+        for (int i = 0; i < intersectionChecks; i++)
+        {
+            var args = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var firstRectangle = rectangles.FirstOrDefault(r => r.Id == args[0]);
+            var secondRectangle = rectangles.FirstOrDefault(r => r.Id == args[1]);
+
+            if (firstRectangle == null || secondRectangle == null)
+            {
+                Console.WriteLine("false");
+            }
+            else
+            {
+                Console.WriteLine(firstRectangle.IntersectsRectangle(secondRectangle) ? "true" : "false");
+            }
+        }
+
+    }
+}
